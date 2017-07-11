b3987b70b1f33c2d8ec8af8a34c6cd90817ccc08
Maschini <maschinigames@gmail.com>
2017-07-11 05:57:50 +0200

    start of mapping

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,40 @@
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            //Dates
+            String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyy"));
+            String year = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyy"));
+
             ArrayList listTemps = new ArrayList();
             ArrayList listDates = new ArrayList();
 
-            //Datum Heute
-            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyy");
-            String date = LocalDateTime.now().format(dtf);
+            File jFile = new File(year + ".json");
+
+            //TODO Maps
+            try {
+                if (jFile.exists()) {
+                    Object obj = new JSONParser().parse(new FileReader(year + ".json"));
+                } else {
+
+                }
+            } catch (ParseException pe) {
+                pe.printStackTrace();
+            }
 
             File f = new File(date + ".txt");
             if (!f.exists()) {
